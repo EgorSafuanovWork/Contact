4681988f5847cf698c78543ac96f30c97348f8e6
Fazan4ik <jagannath2006@outlook.com>
2024-03-11 20:47:21 +0200

    Add project files.

---
+++ Notes/Controllers/ContactsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Contacts.Models; 
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+namespace Notes.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private static string Filename = "contacts.txt";
+        private static Random Rand = new();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var defContacts = new List<Contact>();
+            for (int i = 0; i < Rand.Next(2, 10); i++)
+            {
+                defContacts.Add(new Contact
+                {
+                    Name = $"Name {Rand.Next(1, 100)}",
+                    MobilePhone = $"Phone {Rand.Next(1000, 9999)}",
+                    AlternateMobilePhone = $"Alt Phone {Rand.Next(1000, 9999)}",
+                    Email = $"email{i}@example.com",
+                    ShortDescription = $"Description for {i + 1} contact"
+                });
+            }
+            return View("Index", defContacts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveInFile([FromBody] List<Contact> contacts)
+        {
+            try
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                };
+                string jsonString = JsonSerializer.Serialize(contacts, options);
+                await System.IO.File.WriteAllTextAsync(Filename, jsonString);
+                return Json(new { success = true, message = "Збережено..." });
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false, message = "Помилка..." });
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LoadFromFile()
+        {
+            string jsonString = await System.IO.File.ReadAllTextAsync(Filename);
+            var contactsFromFile = JsonSerializer.Deserialize<List<Contact>>(jsonString);
+            return PartialView("_ContactsPartial", contactsFromFile);
+        }
+    }
+}
---
+++ Notes/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace Contacts.Models
+{
+    [Serializable]
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string MobilePhone { get; set; }
+        public string AlternateMobilePhone { get; set; }
+        public string Email { get; set; }
+        public string ShortDescription { get; set; }
+
+    }
+}
+
+
